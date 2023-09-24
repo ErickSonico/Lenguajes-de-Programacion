3d9aed32cbc44c85126dc9dcf6fa48de928d2b25
Jose Manuel Evangelista Tiburcio <manuel24@ciencias.unam.mx>
2023-09-23 20:55:59 -0600

    Cambio

--- Practica03/src/interp.rkt
+++ Practica03/src/interp.rkt
@@ -6,3 +6,17 @@
 (define (interp expr))
 
 (define (subst sub-id value expr))
+
+; Función para sustituir un identificador por un valor en una expresión
+(define (subst sub-id value expr)
+  ; Verificar si la expresión es una lista
+  (cond
+    [(list? expr)
+     ; Si es una lista, manejar de forma recursiva cada elemento
+     (map (lambda (e) (subst sub-id value e)) expr)]
+    ; Si la expresión coincide con el identificador, sustituir
+    [(eq? expr sub-id)
+     value]
+    ; De lo contrario, devolver la expresión tal cual
+    [else
+     expr]))
--- Practica03/src/parser.rkt
+++ Practica03/src/parser.rkt
@@ -3,3 +3,56 @@
 (require "grammars.rkt")
 
 (define (parse s-exp))
+
+; Función para analizar sintácticamente una expresión s-exp
+(define (parse s-exp)
+  ; Verificar si la expresión es una lista
+  (cond
+    [(list? s-exp)
+     ; Si es una lista, extraer la operación y los argumentos
+     (let ([op (first s-exp)]
+           [args (rest s-exp)])
+       (cond
+         ; Manejar el caso especial de la operación 'with'
+         [(eq? op 'with)
+          (let ([bindings (first args)]
+                [body (second args)])
+            ; Verificar si hay identificadores duplicados en los bindings
+            (if (has-duplicate-bindings? bindings)
+                (error "parse: El identificador está declarado más de una vez")
+                (with (map parse-binding bindings) (parse body))))
+          ]
+         ; Verificar las funciones con aridad 1
+         [(or (eq? op 'sub1) (eq? op 'add1) (eq? op 'not) (eq? op 'zero?) (eq? op 'num?) 
+              (eq? op 'str?) (eq? op 'bool?) (eq? op 'str-length))
+          (if (= (length args) 1)
+              (op (procedure op) (map parse args))
+              (error "parse: La operación ~a espera 1 argumento. Número de argumentos dados: ~a." op (length args)))
+          ]
+         ; Verificar las funciones con aridad 2
+         [(or (eq? op 'modulo) (eq? op 'expt))
+          (if (= (length args) 2)
+              (op (procedure op) (map parse args))
+              (error "parse: La operación ~a espera 2 argumentos. Número de argumentos dados: ~a." op (length args)))
+          ]
+         ; Para otras funciones, simplemente verificar que no estén vacías
+         [else
+          (if (> (length args) 0)
+              (op (procedure op) (map parse args))
+              (error "parse: La operación ~a espera al menos 1 argumento." op))
+          ]))]
+    ; Si s-exp no es una lista, simplemente devolverla
+    [else
+     s-exp]))
+
+; Función para verificar si hay identificadores duplicados en los bindings
+(define (has-duplicate-bindings? bindings)
+  (let ([ids (map first bindings)])
+    (not (= (length ids) (length (remove-duplicates ids))))))
+
+; Función para analizar un binding
+(define (parse-binding binding)
+  ; Verificar la estructura del binding
+  (if (= (length binding) 2)
+      (binding (first binding) (parse (second binding)))
+      (error "parse: Estructura de binding incorrecta: ~a" binding)))
