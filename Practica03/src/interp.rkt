57b7d02892ceada7a8761ff71787410066f9456c
Erick García <erick_sonico@ciencias.unam.mx>
2023-09-27 00:22:01 -0600

    Intérprete

--- Practica03/src/interp.rkt
+++ Practica03/src/interp.rkt
@@ -3,7 +3,35 @@
 (require "grammars.rkt")
 (require "parser.rkt")
 
-;(define (interp expr))
+;; Función interp del lenguaje para el análisis semántico.
+(define (interp expr)
+  (match expr
+    [(id i) (error 'interp "Identificador libre")]
+    ; Si recibe id, num, bool o strinG simplemente las regresa
+    [(num expr) expr]
+    [(bool expr) expr]
+    [(strinG expr) expr]
+    ; si la WAE es una op, aplica la expr a cada elemento de args
+    [(op expr args) (apply expr (map(lambda(expr)(interp expr)) args))]
+    [(with assigns body) 
+            (interp (foldl
+                (lambda (bdg expr-res)
+                    (subst (binding-id bdg) (binding-value bdg) expr-res)
+                )
+                (with-body expr)
+                (with-assigns expr))
+            )
+        ]
+    [(with* (cons (binding id value) cdr-bindings) body)
+     (if (empty? cdr-bindings)
+         (interp (subst body id value))
+         (interp (subst (with* cdr-bindings body)
+                        id
+                        (num (interp value)))))]
+    ; Si la expresión no es ninguna de las anteriores, interp no puede trabajar con ella
+    ; por lo que regresa un error
+    [else (error 'interp "La expresión no es válida")]
+    ))
 
 ; Función para sustituir un identificador por un valor en una expresión
 (define (subst sub-id value expr)
